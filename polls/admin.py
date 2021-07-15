92b294b212aa01ee36e10f39a5790cbd84ce3157
吴林哲 <linzhe@yunshan.net>
2021-07-15 16:12:24 +0800

    自定义后台表单

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 from .models import Choice
-admin.site.register(Question)
+
+
+# 自定义后台表单
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#
+#
+# admin.site.register(Question, QuestionAdmin)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)

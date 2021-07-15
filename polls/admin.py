77c2cf42f02be70c925702048ff16c0ca810277b
吴林哲 <linzhe@yunshan.net>
2021-07-15 16:18:28 +0800

    自定义后台表单

--- polls/admin.py
+++ polls/admin.py
@@ -1,22 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question
-from .models import Choice
+from .models import Choice, Question
 
 
-# 自定义后台表单
-# class QuestionAdmin(admin.ModelAdmin):
-#     fields = ['pub_date', 'question_text']
-#
-#
-# admin.site.register(Question, QuestionAdmin)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
